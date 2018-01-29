afc8ec764d7ba1e0c6961849bbd419507a69eb8b
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2018-01-30 03:18:51 +0900

    Add not found error

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ extern crate rocket_contrib;
 use std::collections::HashMap;
 use std::path::{Path, PathBuf};
 
+use rocket::Request;
 use rocket::response::NamedFile;
 use rocket_contrib::Template;
 
@@ -23,9 +24,18 @@ fn assets(file: PathBuf) -> Option<NamedFile> {
     NamedFile::open(Path::new("static/").join(file)).ok()
 }
 
+#[error(404)]
+fn not_found(req: &Request) -> Template {
+    let mut ctx = HashMap::new();
+    ctx.insert("path", req.uri().as_str());
+    Template::render("errors/404", &ctx)
+}
+
 fn rocket() -> rocket::Rocket {
     rocket::ignite()
         .mount("/", routes![index, assets])
+        .attach(Template::fairing())
+        .catch(errors![not_found])
 }
 
 fn main() {
