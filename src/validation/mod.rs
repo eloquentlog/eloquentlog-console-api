9cacdb613a4bb1ed1c5418e9700098d5a2f05a6d
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2019-04-17 11:19:25 +0900

    Update username validations

--- src/validation/mod.rs
+++ src/validation/mod.rs
@@ -10,6 +10,7 @@ max as original_max,
 type SV = Box<Fn(&String) -> ValidatorResult>;
 
 const DIGITS: &[char] = &['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const SIGNS: &[char] = &['_'];
 
 pub fn alphanumeric_underscore_if_present(
 ) -> Box<Fn(&Option<String>) -> ValidatorResult> {
@@ -26,11 +27,13 @@ pub fn not_contain_only_digits_or_underscore_if_present(
     Box::new(move |s: &Option<String>| {
         match &s {
             Some(v) => {
-                for c in v.replace("_", "").chars() {
-                    if !DIGITS.contains(&c) {
+                dbg!(v);
+                for c in v.chars() {
+                    if !DIGITS.contains(&c) && !SIGNS.contains(&c) {
                         return Ok(());
                     }
                 }
+                dbg!(v);
                 Err(Invalid {
                     msg: "Must not contain only digits or underscore"
                         .to_string(),
@@ -76,6 +79,42 @@ pub fn not_overlap_with(field: &'static str) -> Box<Fn(Option<String>) -> SV> {
             }
             Ok(())
         })
+    })
+}
+
+pub fn not_start_with_if_present(
+    needle: &'static str,
+) -> Box<Fn(&Option<String>) -> ValidatorResult> {
+    Box::new(move |s: &Option<String>| {
+        match &s {
+            Some(v) if !v.is_empty() && v.replacen(needle, "", 1) == v[1..] => {
+                Err(Invalid {
+                    msg: "Must not start with '%1'".to_string(),
+                    args: vec![needle.to_string()],
+                    human_readable: format!("Must not start with '{}'", needle),
+                })
+            },
+            _ => Ok(()),
+        }
+    })
+}
+
+pub fn not_start_with_digits_if_present(
+) -> Box<Fn(&Option<String>) -> ValidatorResult> {
+    Box::new(move |s: &Option<String>| {
+        match &s {
+            Some(v) if !v.is_empty() => {
+                if !DIGITS.contains(&v.chars().next().unwrap()) {
+                    return Ok(());
+                }
+                Err(Invalid {
+                    msg: "Must not start with digits".to_string(),
+                    args: vec![],
+                    human_readable: "Must not start with digits".to_string(),
+                })
+            },
+            _ => Ok(()),
+        }
     })
 }
 
