2bfa5a10a6d999113d14703318d58613ae0ca441
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2019-05-10 18:12:05 +0900

    Fix format warnings reported by 1.36.0-nightly

--- src/validation/user.rs
+++ src/validation/user.rs
@@ -5,12 +5,9 @@ use diesel::PgConnection;
 use rocket_contrib::json::Json;
 
 use validation::{
-    alphanumeric_underscore_if_present,
-    max_if_present,
-    not_contain_only_digits_or_underscore_if_present,
-    not_overlap_with,
-    not_start_with_digits_if_present,
-    not_start_with_if_present,
+    alphanumeric_underscore_if_present, max_if_present,
+    not_contain_only_digits_or_underscore_if_present, not_overlap_with,
+    not_start_with_digits_if_present, not_start_with_if_present,
 };
 use request::User as RequestData;
 use model::user::{NewUser, User};
