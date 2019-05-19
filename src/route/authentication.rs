35aa605a1e7c8611368131e13e37f8a98a0c1354
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2019-05-20 04:01:49 +0900

    Add JWT

--- src/route/authentication.rs
+++ src/route/authentication.rs
@@ -1,18 +1,28 @@
+use rocket::State;
 use rocket::http::Status;
 use rocket_slog::SyncLogger;
 
+use config::Config;
 use db::DbConn;
 use model::user::User;
 use request::UserLogin as RequestData;
 use response::Response;
 
 #[post("/login", data = "<data>", format = "json")]
-pub fn login(conn: DbConn, data: RequestData, logger: SyncLogger) -> Response {
+pub fn login(
+    conn: DbConn,
+    data: RequestData,
+    logger: SyncLogger,
+    state: State<Config>,
+) -> Response
+{
     let res: Response = Default::default();
 
     match User::find_by_email_or_username(&data.username, &conn) {
         Some(ref user) if user.verify_password(&data.password) => {
-            res.format(json!({ "message": "Success" }))
+            // TODO
+            let token = user.to_jwt(&state.jwt_issuer, &state.jwt_secret);
+            res.format(json!({ "message": token.to_string() }))
         },
         _ => {
             warn!(logger, "login failed: username {}", data.username);
--- src/route/top.rs
+++ src/route/top.rs
@@ -1,10 +1,12 @@
+use rocket::State;
 use rocket::http::Status;
 use rocket_slog::SyncLogger;
 
+use config::Config;
 use response::Response;
 
 #[get("/")]
-pub fn index(_logger: SyncLogger) -> Response {
+pub fn index(_logger: SyncLogger, _state: State<Config>) -> Response {
     let res = Response {
         status: Status::Ok,
         data: json!(null),
