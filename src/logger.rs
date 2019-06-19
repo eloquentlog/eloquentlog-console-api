eddd8a5e0ba535911ce6ca1ff9478d5c572d4b8c
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2019-06-19 11:41:54 +0900

    Update logger severities
    * development: debug
    * production:  error
    * testing:     critical -> warning
    * other:       error -> trace

--- src/logger.rs
+++ src/logger.rs
@@ -13,11 +13,66 @@ pub fn get_logger(config: &Config) -> Logger {
 
     let level = match config.env_name {
         "development" => Severity::Debug,
-        "testing" => Severity::Critical,
-        _ => Severity::Error,
+        "production" => Severity::Error,
+        "testing" => Severity::Warning,
+        _ => Severity::Trace,
     };
 
     builder.level(level);
     builder.destination(Destination::Stdout);
     builder.build().unwrap()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    use slog::Drain;
+
+    use model::test::run;
+
+    #[test]
+    fn test_get_logger_development() {
+        run(|_, _, _| {
+            let c = Config::from("development").unwrap();
+            let logger = get_logger(&c);
+
+            assert!(logger.is_critical_enabled());
+            assert!(logger.is_error_enabled());
+            assert!(logger.is_warning_enabled());
+            assert!(logger.is_info_enabled());
+            assert!(logger.is_debug_enabled());
+            assert!(!logger.is_trace_enabled());
+        })
+    }
+
+    #[test]
+    fn test_get_logger_production() {
+        run(|_, _, _| {
+            let c = Config::from("production").unwrap();
+            let logger = get_logger(&c);
+
+            assert!(logger.is_critical_enabled());
+            assert!(logger.is_error_enabled());
+            assert!(!logger.is_warning_enabled());
+            assert!(!logger.is_info_enabled());
+            assert!(!logger.is_debug_enabled());
+            assert!(!logger.is_trace_enabled());
+        })
+    }
+
+    #[test]
+    fn test_get_logger_test() {
+        run(|_, _, _| {
+            let c = Config::from("testing").unwrap();
+            let logger = get_logger(&c);
+
+            assert!(logger.is_critical_enabled());
+            assert!(logger.is_error_enabled());
+            assert!(logger.is_warning_enabled());
+            assert!(!logger.is_info_enabled());
+            assert!(!logger.is_debug_enabled());
+            assert!(!logger.is_trace_enabled());
+        })
+    }
 }
