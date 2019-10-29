030c6e638fb9c48a530347193e0c9a29214f9dd7
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2019-10-29 12:33:47 +0900

    Improve error handling for authentication token
    * Create macros for response (outcome) building

--- src/request/mod.rs
+++ src/request/mod.rs
@@ -4,3 +4,33 @@ pub mod token;
 pub mod user;
 
 type ID = usize;
+
+#[macro_export]
+macro_rules! bad_request_by {
+    ($reason:expr) => {
+        ::rocket::request::Outcome::Failure((
+            ::rocket::http::Status::BadRequest,
+            $reason,
+        ))
+    };
+}
+
+#[macro_export]
+macro_rules! not_found_by {
+    ($reason:expr) => {
+        ::rocket::request::Outcome::Failure((
+            ::rocket::http::Status::NotFound,
+            $reason,
+        ))
+    };
+}
+
+#[macro_export]
+macro_rules! unprocessable_entity_by {
+    ($reason:expr) => {
+        ::rocket::request::Outcome::Failure((
+            ::rocket::http::Status::UnprocessableEntity,
+            $reason,
+        ))
+    };
+}
